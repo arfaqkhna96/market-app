615424cb8989866a4a7a54a673c8e76c38ac7719
arfaqkhna96 <105111298+arfaqkhna96@users.noreply.github.com>
2025-01-06 18:02:36 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-# this file is being executed in /opt/codedeploy-agent/deployment-root/47../<deployment_id>/
+# This file is being executed in /opt/codedeploy-agent/deployment-root/47../<deployment_id>/
+# Stdout logs of this process executing can be found in /opt/codedeploy-agent/deployment-root/47../<deployment_id>/logs/scripts.log
 
-#stdout logs of this process executing can be found in /opt/codedeploy-agent/deployment-root/47../<deployment_id>/logs/scripts.log
-
-# here we update the server and install node and npm
-echo installing dependencies
+# Here we update the server and install node and npm
+echo "Installing dependencies"
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt-get -y install npm
 
-# check to make sure the symbolic link for nodejs node exists
-echo checking for nodejs symlink
+# Check to make sure the symbolic link for nodejs node exists
+echo "Checking for nodejs symlink"
 file="/usr/bin/node"
-if [ -f $file ] && [ ! -L $file ] ; then
+if [ -f $file ] && [ ! -L $file ]; then
   echo "$file exists and is not a symlink"
   sudo ln -s /usr/bin/nodejs
 else
   echo "$file exists and is already a symlink"
 fi
 
-# install the application using npm
-# we need to traverse to where the application bundle is copied too.
-echo installing application with npm
+# Install the application using npm
+# We need to traverse to where the application bundle is copied to.
+echo "Installing application with npm"
 cd /var/www/
 sudo npm install
 
-echo installing pm2
+echo "Installing pm2"
 sudo npm install pm2 -g
