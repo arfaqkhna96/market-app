fbf1086747b8bdd33d973bad6cc04f2af58482c2
arfaqkhna96 <105111298+arfaqkhna96@users.noreply.github.com>
2025-01-15 14:04:14 +0530

    Update deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,37 +1,15 @@
 @echo off
 
-echo The following "npm" command builds your Node.js/React application for
-echo production in the local "build" directory (i.e. within the
-echo "C:\ProgramData\Jenkins\.jenkins\workspace\simple-node-js-react-npm-app" directory),
-echo correctly bundles React in production mode and optimizes the build for
-echo the best performance.
-echo on
+echo Building the application...
 npm run build
-echo off
-
-echo The following "npm" command runs your Node.js/React application in
-echo development mode and makes the application available for web browsing.
-echo The "npm start" command has a trailing ampersand so that the command runs
-echo as a background process (i.e. asynchronously). Otherwise, this command
-echo can pause running builds of CI/CD applications indefinitely. "npm start"
-echo is followed by another command that retrieves the process ID (PID) value
-echo of the previously run process (i.e. "npm start") and writes this value to
-echo the file ".pidfile".
-echo on
-start /B npm start
-timeout /T 5
-for /f "tokens=2 delims=," %%i in ('tasklist /fi "imagename eq node.exe" /fo csv') do (
-    echo %%i > .pidfile
-    echo Captured PID: %%i
-    goto end
-)
-:end
-echo off
-
-if not exist .pidfile (
-    echo "Error: .pidfile not found."
+if %ERRORLEVEL% NEQ 0 (
+    echo "Build failed. Exiting script."
     exit /b 1
 )
+
+echo Starting the application...
+start /B npm start
+timeout /T 5
 
 echo Now...
 echo Visit http://localhost:3000 to see your Node.js/React application in action.
